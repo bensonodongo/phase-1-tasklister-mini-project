d7fe90ca31d372f42d2230448b2c401e1dd6d885
Benson <benson.odongo@student.moringaschool.com>
2022-09-05 20:09:44 +0300

    mini project completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    handleToDo(e.target.new_task.value)
+    
+   })
+  })
+  
+  
+  function handleToDo(todo){
+    let li = document.createElement('li')
+    li.textContent = todo
+    document.querySelector("#tasks").appendChild(li)
+  }
